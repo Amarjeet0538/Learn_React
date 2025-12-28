1c2bb4d27761c4b437f233b6b600986bc498d4ad
amarjeet0538@ <amarjeet0538@gmail.com>
2025-12-28 11:57:08 +0530

    made code clean

--- react-appp/src/utils/constants.js
+++ react-appp/src/utils/constants.js
@@ -1,35 +1,32 @@
+import { TMDB_API_KEY } from "./api_key";
 
-export const MOVIE_DATA_URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=ac32239504c25e36fedca03b4978db8d`
+export const MOVIE_DATA_URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${TMDB_API_KEY}`;
 
-export const SEARCH_URL = "https://api.themoviedb.org/3/search/multi?api_key=ac32239504c25e36fedca03b4978db8d&query="
-
-
-
-export const IMDB_API_KEY = "ac32239504c25e36fedca03b4978db8d"
+export const SEARCH_URL = `https://api.themoviedb.org/3/search/multi?api_key=${TMDB_API_KEY}&query=`;
 
 export const LANGUAGE_MAP = {
-  en: "English",
-  ja: "Japanese",
-  hi: "Hindi",
-  ko: "Korean",
-  fr: "French",
-  fi: "Finnish",
-  es: "Spanish",
-  de: "German",
-  it: "Italian",
-  zh: "Chinese",
-  ru: "Russian",
-  pt: "Portuguese",
-  ar: "Arabic",
-  tr: "Turkish",
-  th: "Thai",
-  id: "Indonesian",
-  nl: "Dutch",
-  sv: "Swedish",
-  pl: "Polish",
-  ta: "Tamil",
-  te: "Telugu",
-  ml: "Malayalam",
-  bn: "Bengali",
-  ur: "Urdu",
+	en: "English",
+	ja: "Japanese",
+	hi: "Hindi",
+	ko: "Korean",
+	fr: "French",
+	fi: "Finnish",
+	es: "Spanish",
+	de: "German",
+	it: "Italian",
+	zh: "Chinese",
+	ru: "Russian",
+	pt: "Portuguese",
+	ar: "Arabic",
+	tr: "Turkish",
+	th: "Thai",
+	id: "Indonesian",
+	nl: "Dutch",
+	sv: "Swedish",
+	pl: "Polish",
+	ta: "Tamil",
+	te: "Telugu",
+	ml: "Malayalam",
+	bn: "Bengali",
+	ur: "Urdu",
 };
